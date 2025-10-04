15dd1bbf0acd9c7ad4620df1e7bcebfb8c8ef1bf
creator-uiux <creatorbusinesswork@gmail.com>
2025-10-05 03:20:29 +0800

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'barangaylink/php-app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cef861f8ac65710b441a6cee305285339b6deb87',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'barangaylink/php-app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cef861f8ac65710b441a6cee305285339b6deb87',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
