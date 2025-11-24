69ef43151f35bc5bfb3d72e41ff363a7e20d81ac
creator-uiux <creatorbusinesswork@gmail.com>
2025-11-24 12:21:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set default port if not set
+PORT=${PORT:-80}
+
 # Generate application key if not set
 if [ -z "$APP_KEY" ]; then
     echo "APP_KEY not set, generating..."
@@ -8,6 +11,10 @@ fi
 
 # Run database migrations
 php artisan migrate --force
+
+# Configure Apache to listen on the specified port
+sed -i "s/80/$PORT/g" /etc/apache2/ports.conf
+sed -i "s/:80/:$PORT/g" /etc/apache2/sites-available/000-default.conf
 
 # Start Apache in foreground
 apache2-foreground
